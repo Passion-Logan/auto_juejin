1f52aac3bcff10012aea3dffd81fa76d7de3368b
Passion-Logan <wangdadamailbox@163.com>
2021-09-06 11:10:19 +0800

    first commit

---
+++ src/main/java/com/auto/cody/CodyApplication.java
@@ -0,0 +1,13 @@
+package com.auto.cody;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CodyApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CodyApplication.class, args);
+    }
+
+}
